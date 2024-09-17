1c0575e96577bd43686ccd7ba509e8742b9aec14
PoshiloySmih <mrforxbox@mail.ru>
2024-09-17 11:34:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/Course2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course2/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
